ac2a006ac27db1ac929db322e6735c91da4fc657
jorge <bandala1226@gmail.com>
2025-08-02 14:31:01 -0600

    Agregar archivos y carpetas necesarios para el proyecto

---
+++ src/main/java/com/coleccion/serviciosusuarios/serviciosUsuarios/ServiciosUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.coleccion.serviciosusuarios.serviciosUsuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiciosUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiciosUsuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coleccion/serviciosusuarios/serviciosUsuarios/ServiciosUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coleccion.serviciosusuarios.serviciosUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiciosUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
